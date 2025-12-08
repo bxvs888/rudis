258a68014afce2b0a84b8f282ce07e2597e736f2
就眠儀式 <854085467@qq.com>
2025-12-08 14:15:07 +0800

    update tests

--- tests/test_cmd.rs
+++ tests/test_cmd.rs
@@ -25,6 +25,22 @@ mod tests {
     }
 
     #[test]
+    fn test_set_batch() {
+        let mut con = setup();
+        for i in 0..100000 {
+            let _: () = con.set(i.to_string(), i.to_string()).unwrap();
+        }
+    }
+
+    #[test]
+    fn test_get_batch() {
+        let mut con = setup();
+        for _i in 0..100000 {
+            let _: () = con.get("user").unwrap();
+        }
+    }
+
+    #[test]
     fn test_del() {
         
         let mut con = setup();
